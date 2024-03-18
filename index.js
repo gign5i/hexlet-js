0a653d7913edc98f1aeb12f66883c926d992f75f
Benamgkhar <swsbbnml@gmail.com>
2024-03-19 00:33:44 +0300

    add index.js; src/index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-import _ from "lodash";
-console.log(_.last(["one", "two"]));
+import { last } from "./src/index.js";
 
-let a = 0;
-let b = 1;
-if (a < b) {
-  console.log("a<b");
-}
+last();
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { last } from "../node_modules/lodash/last"; //импорт last из lodash
+console.log(last(["one", "two"]));
+export { last };
